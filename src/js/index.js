d26bae19bfed628fc4969a38e2aaaf37bfb7edd6
UCMessina <ucmessina@alumnostrazos.net>
2025-01-15 11:54:46 +0100

    +FirstCommit

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const hambuergerButton = document.getElementById('hamburger-icon');
+const spotElement = document.getElementById('header__spot');
+
+const ShowMenu = (e) => {
+  console.log(e);
+};
+
+hambuergerButton.addEventListener('click', ShowMenu);
